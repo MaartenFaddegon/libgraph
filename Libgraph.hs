cebb7c5011e73ecface4038629585fb4d8c4498b
Maarten Faddegon <mf357@kent.ac.uk>
2014-06-08 13:39:48 +0100

    quadratic dominance implementation

--- Libgraph.hs
+++ Libgraph.hs
@@ -2,7 +2,10 @@ module Libgraph
 ( Arc(..), Graph(..)
 )
 where
+import Data.Maybe
+import Data.List
 
+--------------------------------------------------------------------------------
 -- External representation of graphs
 
 data Arc vertex   = Arc   { source :: vertex
@@ -35,3 +38,26 @@ dfs g (v:stack) vs = if v `elem` vs
                      else dfs g (succs g v ++ stack) (v:vs)
 
 --------------------------------------------------------------------------------
+-- Dominance
+
+type Domsets vertex = [(vertex,[vertex])]
+
+dominators :: Eq vertex => vertex -> Domsets vertex -> [vertex]
+dominators = lookup'
+
+domsets :: Eq vertex => Graph vertex -> Domsets vertex
+domsets g = dom g domset0
+  where domset0 = map (\v -> (v, if v == r then [r] else vs)) vs
+        vs      = vertices g
+        r       = root g
+
+dom :: Eq vertex => Graph vertex -> Domsets vertex -> Domsets vertex
+dom g ds = if ds' == ds then ds else dom g ds'
+  where ds'       = map (\(v,_) -> (v, nextSet v)) ds
+        nextSet v = foldl (\s p -> (dominators p ds) `intersect` s) [] (preds g v)
+
+--------------------------------------------------------------------------------
+-- Some helper functions
+
+lookup' :: Eq a => a -> [(a, b)] -> b
+lookup' x ys = fromJust (lookup x ys)
