2d37b17e3d7fd7e0a16e1868b7f71bab6b80e7a5
Maarten Faddegon <mf357@kent.ac.uk>
2014-07-14 17:11:47 +0100

    cleanup some code

--- Data/Graph/Libgraph/Dagify.hs
+++ Data/Graph/Libgraph/Dagify.hs
@@ -8,27 +8,27 @@ dagify :: Ord vertex => ([vertex]->vertex) -> Graph vertex -> Graph vertex
 dagify merge = (collapse merge) . remove
 
 remove :: Ord v => Graph v -> Graph v
-remove g = filterArc (\a@(Arc _ tgt) -> not $ isBackEdge a && isRedHead tgt) g
-  where isRedHead  h = h `elem` getRedHeaders (getCycleNest g)
-        isBackEdge a = getEdgetype (getDfs g) a == BackEdge
+remove g = filterArc (\a -> not $ isBackEdge a && hasRedHead a) g
+  where isBackEdge a = getEdgetype (getDfs g) a == BackEdge
+        hasRedHead (Arc _ h) = h `elem` getRedHeaders (getCycleNest g)
 
 collapse :: Ord v => ([v]->v) -> Graph v -> Graph v
-collapse merge g = foldl merge' g ics
+collapse merge g = foldl collapseCycle g ics
   where (CycleTree _ ts) = getCycles (getCycleNest g)
         ics              = filter (\c -> case c of Irreducible _ -> True; _ -> False) ts
-        merge' g (Irreducible cts)
-          = let ws = (map (\(CycleTree v []) -> v) cts)
+        collapseCycle g (Irreducible cts)
+          = let ws = (verticesInCycle cts)
                 v  = (merge ws)
             in rewire g ws v
+        verticesInCycle = map (\(CycleTree v []) -> v)
 
 rewire :: Eq vertex => Graph vertex -> [vertex] -> vertex -> Graph vertex
 rewire (Graph r vs as) ws c 
   = Graph r 
           (c : filter (not . (`elem` ws)) vs)
-          (nub $ map fromTo $ filter (not . internalArc) as)
-  where internalArc (Arc src tgt) = src `elem` ws && tgt `elem` ws
+          (nub $ map fromTo $ filter (not . isInternalArc) as)
+  where isInternalArc (Arc src tgt) = src `elem` ws && tgt `elem` ws
         fromTo (Arc src tgt)
           | tgt `elem` ws = Arc src c
           | src `elem` ws = Arc c   tgt
           | otherwise     = Arc src tgt
-        
