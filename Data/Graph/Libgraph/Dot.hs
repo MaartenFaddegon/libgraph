271afbd475cde1731987ec9fabc5a26b677aaa6a
Maarten Faddegon <mf357@kent.ac.uk>
2015-10-01 10:29:54 +0100

    when rendering graphs, do not handle the root node in a special way but leave this to the user to decide

--- Data/Graph/Libgraph/Dot.hs
+++ Data/Graph/Libgraph/Dot.hs
@@ -11,7 +11,7 @@ showWith :: Eq vertex => Graph vertex arc -> (vertex->(String,String))
                           -> (Arc vertex arc->String) -> String
 showWith g vLabel aLabel
   = "diGraph G {\n"
-  ++ vName r ++ "[shape=none label=\".\"]\n"
+  -- ++ vName r ++ "[shape=none label=\".\"]\n"
   ++ foldl (\s v -> show1 v ++ s) "" vs
   ++ foldl (\s a -> (showArc vs aLabel a) ++ s) "" (arcs g)
   ++ "}\n"
@@ -20,7 +20,7 @@ showWith g vLabel aLabel
         isRoot (v,_) = v == root g
 
         show1 v
-          | isRoot v  = vName r ++ "[shape=none label=\".\"]\n"
+          -- | isRoot v  = vName r ++ "[shape=none label=\".\"]\n"
           | otherwise = (showVertex vLabel v)
 
 showVertex :: (vertex->(String,String)) -> (vertex,Int) -> String
@@ -39,10 +39,17 @@ vName :: Int -> String
 vName i = "v" ++ show i
 
 escape :: String -> String
-escape []          = []
-escape ('"' : ss)  = '\\' : '"'   : escape ss
-escape ('\\' : ss)  = '\\' : '\\' : escape ss
-escape (s   : ss)  = s : escape ss
+escape [] = []
+escape ss = escape' False ss
+
+escape' _ []                          = []
+escape' _ ('"'  : ss)                 = '\\' : '"'  : escape' False ss
+escape' _ ('\\' : ss)                 = '\\' : '\\' : escape' False ss
+escape' w (s    : ss)
+ | w       && (s == ' ' || s == '\n') =               escape' True  ss
+ | (not w) && (s == ' ' || s == '\n') =         ' ' : escape' True  ss
+ | otherwise                          =         s   : escape' False ss
+
 
 -- | Invoke Graphviz and Imagemagick to display graph on screen.
 display :: (Graph vertex arc -> String) -> Graph vertex arc -> IO ()
